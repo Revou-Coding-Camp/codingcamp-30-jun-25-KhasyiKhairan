33b4e345d8c93c2a898d4ab3f3a931de73b84556
Muhammad Khasyi Khairan <khasyi.khairan@gmail.com>
2025-07-05 11:02:55 +0700

    tambahkan valdasi js

--- js/script.js
+++ js/script.js
@@ -13,47 +13,18 @@ setInterval(tampilkanWaktu, 1000);
 tampilkanWaktu(); // jalankan pertama kali agar tidak delay
 
 
+function validateForm () {
+  const nama = document.getElementById('nama').value;
+  const tanggal_lahir = document.getElementById('tanggal_lahir').value;
+  const jenis_kelamin = document.getElementById('jenis_kelamin').value;
+  const nomor_hp = document.getElementById('nomor_hp').value;
+  const pesan = document.getElementById('pesan').value;
 
-
-document.getElementById("formData").addEventListener("submit", function(event) {
-  event.preventDefault(); // Mencegah reload halaman
-
-  // Ambil nilai input
-  const nama = document.getElementById("nama").value;
-  const tgllahir = document.getElementById("tanggal_lahir").value;
-  const jnskel = document.getElementById("jenis_kelamin").value;
-  const notelp = document.getElementById("nomor_hp").value;
-  const pesan = document.getElementById("pesan").value;
-
-  alert(nama);
-
-  // Tampilkan hasilnya di elemen HTML
-  const hasil = `
-    <div class="row align-items-center">
-      <label for="nama" class="col-sm-3 col-form-label">Name</label>
-      <div class="col-sm-9">${nama}</div>
-    </div>
-
-    <div class="row align-items-center">
-      <label for="tanggal_lahir" class="col-sm-3 col-form-label">Date of Birth</label>
-      <div class="col-sm-9">${tgllahir}</div>
-    </div>
-
-    <div class="row">
-      <label class="col-sm-3 col-form-label">Gender</label>
-      <div class="col-sm-9">${jnskel}</div>
-    </div>
-
-    <div class="row align-items-center">
-      <label for="nomor_hp" class="col-sm-3 col-form-label">Phone Number</label>
-      <div class="col-sm-9">${notelp}</div>
-    </div>
-
-    <div class="row mb-3">
-      <label for="pesan" class="col-sm-3 col-form-label">Message</label>
-      <div class="col-sm-9">${pesan}</div>
-    </div>
-  `;
-    document.getElementById("hasilSubmit").innerHTML = hasil;
-});
+  document.getElementById('res-nama').innerHTML =  nama;
+  document.getElementById('res-tanggal_lahir').innerHTML =  tanggal_lahir;
+  document.getElementById('res-jenis_kelamin').innerHTML =  jenis_kelamin;
+  document.getElementById('res-nomor_hp').innerHTML =  nomor_hp;
+  document.getElementById('res-pesan').innerHTML =  pesan;
+  //alert(nama);
+}
 
